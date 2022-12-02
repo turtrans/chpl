f09ee310d3c7141c8f4946e7d2f3732bf1a7048c
Jesus, ACFS <54296154+annacfs@users.noreply.github.com>
2022-12-02 16:09:22 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CHPL'
+copyright = '2022, ACFSJ'
+author = 'JESUS, ACFS'
 
 release = '0.1'
 version = '0.1.0'
